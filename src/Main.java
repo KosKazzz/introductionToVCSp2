f150c66cfc5f619e8292516926b48feca716018b
KosKazzz <xuzzzz@gmail.com>
2022-07-27 23:07:30 +0300

    min and truncatedMean methods added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager manager = new SalesManager(new long[]{550, 610, 400, 100, 132, 98, 556, 45});
-        System.out.println("Best sale : " + manager.max());
+        SalesManager manager = new SalesManager(new int[]{550, 610, 400, 100, 132, 98, 556, 45}); //summ = 2491
+        System.out.println(" Best sale : " + manager.max());
+        System.out.println(" Worse sale : " + manager.min());
+        System.out.println(" Truncated sales value : "+ manager.truncatedMean() ); // 306
 
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,39 @@
 public class SalesManager {
-    protected long[] sales;
+    protected int[] sales;
 
-    public SalesManager(long[] sales) {
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    public long max() {
-        long max = -1;
-        for (long sale : sales) {
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
+    }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int truncatedMean() {
+        int trMean = 0;
+        for (int sale : sales) {
+            if (sale == this.max() || sale == this.min()) {
+                continue;
+            }
+            trMean += sale;
+        }
+        return trMean / (sales.length - 2);
+
     }
 }
