8eae71655d1a204430b04ef75e4c046f0229d634
KosKazzz <xuzzzz@gmail.com>
2022-07-27 21:40:33 +0300

    change int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager manager = new SalesManager(new int[]{550, 610, 400, 100, 132, 98, 556, 45});
+        SalesManager manager = new SalesManager(new long[]{550, 610, 400, 100, 132, 98, 556, 45});
         System.out.println("Best sale : " + manager.max());
 
     }
